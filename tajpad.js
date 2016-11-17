621558f3b29e53896c5961379b4bba388b619f50
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-11-17 18:51:29 +0100

    Create the words as spans and highlight them
    The words are now highlighted as the user types depending on whether
    it is correct or not.

--- tajpad.js
+++ tajpad.js
@@ -1,3 +1,9 @@
+var span_list = [];
+var word_list = [];
+var next_word = 0;
+var end_word_re = /[^ ] /;
+var word_ok = true;
+
 function get_word()
 {
   var score = Math.random();
@@ -15,16 +21,58 @@ function get_word()
   return words[min * 2];
 }
 
-function fill_words()
+function fill_words(amount)
 {
   var i;
-  var words = [];
-
-  for (i = 0; i < 1000; i++)
-    words.push(get_word());
-
   var p = document.getElementById("word-list");
-  p.appendChild(document.createTextNode(words.join(" ")));
+
+  for (i = 0; i < amount; i++) {
+    p.appendChild(document.createTextNode(" "));
+    var word = get_word();
+    word_list.push(word);
+
+    var span = document.createElement("span");
+    span_list.push(span);
+    span.appendChild(document.createTextNode(word));
+    p.appendChild(span);
+  }
 }
 
-window.onload = fill_words;
+function word_changed(word_input)
+{
+  var value = word_input.value;
+
+  if (end_word_re.test(value)) {
+    value = value.substring(0, value.length - 1);
+
+    var result = value == word_list[next_word] ? "good" : "bad";
+    span_list[next_word].className = result;
+
+    next_word++;
+    span_list[next_word].className = "next";
+
+    word_input.value = "";
+    word_ok = true;
+  } else if (word_list[next_word].substring(0, value.length) == value) {
+    if (!word_ok) {
+      span_list[next_word].className = "next";
+      word_ok = true;
+    }
+  } else {
+    if (word_ok) {
+      span_list[next_word].className = "error";
+      word_ok = false;
+    }
+  }
+}
+
+function initialise()
+{
+  fill_words(100);
+  span_list[0].className = "next";
+
+  var word_input = document.getElementById("word-input");
+  word_input.oninput = function() { word_changed(word_input) };
+}
+
+window.onload = initialise;
